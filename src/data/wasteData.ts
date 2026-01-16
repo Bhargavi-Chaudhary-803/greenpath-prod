dbfbda58c3c81f78484aeeb4d0a90c5b07b09943
BHARGAVI CHAUDHARY <bhargavi.2502052537@muj.manipal.edu>
2026-01-17 01:58:40 +0530

    Add files via upload

---
+++ src/data/wasteData.ts
@@ -0,0 +1,71 @@
+export const wasteData = [
+  {
+    id: "1",
+    type: "batteries",
+    title: "5 Car Batteries",
+    location: "Karol Bagh, New Delhi",
+    user: "Raj Malhotra",
+    date: "2 days ago",
+    description:
+      "Old car batteries from garage cleanout. Ready for pickup.",
+  },
+  {
+    id: "2",
+    type: "plastic",
+    title: "Excess Plastic Packaging",
+    location: "Anand Vihar, New Delhi",
+    user: "Samiya Arora.",
+    date: "1 day ago",
+    description:
+      "Large amount of bubble wrap and plastic packaging from recent move.",
+  },
+  {
+    id: "3",
+    type: "electronics",
+    title: "Old TV and Monitor",
+    location: "MG Road, New Celhi",
+    user: "Maanya Sethi.",
+    date: "3 days ago",
+    description:
+      "CRT TV and computer monitor, both non-functional.",
+  },
+  {
+    id: "4",
+    type: "oil",
+    title: "Used Motor Oil (3 gallons)",
+    location: "Pitampura, New Delhi",
+    user: "Tina Oberoi",
+    date: "5 hours ago",
+    description:
+      "Used motor oil in sealed containers from oil change.",
+  },
+  {
+    id: "5",
+    type: "plastic",
+    title: "10+ Plastic Bottles",
+    location: "Malviya Nagar, Jaipur",
+    user: "Akaash Kumar",
+    date: "1 day ago",
+    description:
+      "Clean plastic bottles, sorted and ready for recycling.",
+  },
+  {
+    id: "6",
+    type: "batteries",
+    title: "Household Batteries (AA/AAA)",
+    location: "Bagru, Jaipur",
+    user: "Lucky Ojha",
+    date: "4 days ago",
+    description: "Small bag of used household batteries.",
+  },
+  {
+    id: "7",
+    type: "electronics",
+    title: "Broken Laptop",
+    location: "Raj Nagar Extension, Ghaziabad",
+    user: "Devendra Rajput",
+    date: "2 days ago",
+    description:
+      "Non-working laptop, ready for e-waste recycling.",
+  },
+];
